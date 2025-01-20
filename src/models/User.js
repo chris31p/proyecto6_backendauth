a7546321f1a1f3e8e955bf64cac395a3a5c0c3ce
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-01-20 17:34:53 -0500

    Feat: crear los modelos de la app

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/.+@.+\..+/, "Por favor utiliza un dirección de correo válida"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["comprador", "vendedor"],
+      default: "comprador",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
+
+
